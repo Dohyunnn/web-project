32998653e48f64c5cedb0ab52d1e213212628028
Dohyunnn <qkrehgusqkrehgus@gmail.com>
2022-07-05 19:47:10 +0900

    writeArticle 메서드를 통해 중복 제거

--- src/main/java/com/kor/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/kor/exam/demo/controller/UsrArticleController.java
@@ -9,34 +9,57 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kor.exam.demo.vo.Article;
 
-
 @Controller
 public class UsrArticleController {
 	private int articlesLastId;
 	private List<Article> articles;
-	
+
 	public UsrArticleController() {
 		articlesLastId =0;
 		articles = new ArrayList<>();
+		
+		makeTestData();
 	}
-   @RequestMapping("/usr/article/doAdd")
-   @ResponseBody
-   public Article doAdd(String title, String body) {
-	   int id =articlesLastId +1;
-	   
-	   Article article = new Article(id, title, body);
-	   
-	   articles.add(article);
-	   
-	   articlesLastId = id;
-	   return article;
-   }
-   
-   @RequestMapping("/usr/article/getArticles")
-   @ResponseBody
-   public List<Article> getArticles() {
-	   
-	   return articles;
-   }
-   
+
+	private void makeTestData() {
+		for ( int i =1; i <=10; i++) {
+		String title = "제목" + i;
+		String body = "내용" + i;
+		
+		writeArticle(title,body);
+		}
+	}
+
+	public Article writeArticle(String title, String body) {
+		int id = articlesLastId + 1;
+		Article article = new Article(id, title, body);
+
+		articles.add(article);
+		articlesLastId = id;
+		return article;
+		
+	}
+	
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article  article = writeArticle(title,body);
+
+		return article;
+	}
+
+	@RequestMapping("/usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+
+		return articles;
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+
+		return id + "번 게시물을 삭제하였습니다";
+	}
+
 }
