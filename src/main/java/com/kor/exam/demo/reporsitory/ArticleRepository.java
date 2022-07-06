fba7bef0c6ce8098aae5a42a81d2b86116a92020
Dohyunnn <qkrehgusqkrehgus@gmail.com>
2022-07-06 20:17:40 +0900

    DELETE, UPDATE, SELECET 관련 쿼리를 메서드와 연결

--- src/main/java/com/kor/exam/demo/reporsitory/ArticleRepository.java
+++ src/main/java/com/kor/exam/demo/reporsitory/ArticleRepository.java
@@ -2,9 +2,12 @@ package com.kor.exam.demo.reporsitory;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 import com.kor.exam.demo.vo.Article;
 
 @Mapper
@@ -13,16 +16,19 @@ public interface ArticleRepository {
 	public Article writeArticle(String title, String body);
 	
 	// SELECT * FROM article WHERE id = ?
-	@Select("select * from article where id = #{id}")
+	@Select("SELECET * FROM article where id = #{id}")
 	public Article getArticle(@Param("id") int id);
 
 	// DLETE FROM article WHERE id = ?
+	@Delete("DELETE FROM article WHERE id = (id)")
 	public void deleteArticle(int id);
 
 	// UPDATE article SET title = ?, `body` = ?, updateDate = NOW() WHERE id = ?;
+	@Update("UPDATE article SET title =#{title}, 'body' = #{boy}, updateDate = NOW() WHERE id = #{id}")
 	public void modifyArticle(int id, String title, String body);
 
 	// SELECT * FROM article ORDER BY id DESC;
+	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 }
 
